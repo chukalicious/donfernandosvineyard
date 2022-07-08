a1409b46d7c77c66e4d786cba2cdf5bdafe89ade
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-07-07 23:13:38 -0700

    added header and imported it to app.js

--- donfernando-client/src/App.js
+++ donfernando-client/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import SignUpForm from "./components/SignupForm.js";
+import Header from "./components/Header.js";
 
 function App() {
   return (
     <div>
+      <Header />
       <SignUpForm />{" "}
     </div>
   );
