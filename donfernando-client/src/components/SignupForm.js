3a3abaa10260ca607fbf87ddfd09e073d1acc4a6
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-07-07 18:09:50 -0700

    aligned form inputs

--- donfernando-client/src/components/SignupForm.js
+++ donfernando-client/src/components/SignupForm.js
@@ -2,30 +2,54 @@ import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 
 const SignUpForm = () => {
+  const provisionaryStyle = {
+    marginTop: "1rem",
+  };
   return (
-    <Box component="form">
-      <Box sx={{ paddingTop: "1rem" }}>
-        <TextField
-          required
-          id="firstName"
-          name="firstName"
-          label="First name"
-        />{" "}
-        <TextField required id="lastName" name="lastName" label="Last name" />{" "}
-        <TextField id="phoneNumber" name="phoneNumber" label="Phone Number" />{" "}
-        <TextField
-          required
-          id="emailAddress"
-          name="emailAddress"
-          label="Email Address"
-        />{" "}
-        <TextField
-          required
-          id="homeAddress"
-          name="homeAddress"
-          label="Home Address"
-        />{" "}
-      </Box>
+    <Box
+      component="form"
+      sx={{
+        paddingTop: "1rem",
+        display: "flex",
+        flexDirection: "column",
+        margin: "0 auto",
+        maxWidth: "45%",
+      }}
+    >
+      <TextField
+        style={provisionaryStyle}
+        required
+        id="firstName"
+        name="firstName"
+        label="First name"
+      />{" "}
+      <TextField
+        style={provisionaryStyle}
+        required
+        id="lastName"
+        name="lastName"
+        label="Last name"
+      />{" "}
+      <TextField
+        style={provisionaryStyle}
+        id="phoneNumber"
+        name="phoneNumber"
+        label="Phone Number"
+      />{" "}
+      <TextField
+        style={provisionaryStyle}
+        required
+        id="emailAddress"
+        name="emailAddress"
+        label="Email Address"
+      />{" "}
+      <TextField
+        required
+        style={provisionaryStyle}
+        id="homeAddress"
+        name="homeAddress"
+        label="Home Address"
+      />{" "}
     </Box>
   );
 };
