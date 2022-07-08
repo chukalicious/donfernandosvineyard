1a876d9743ca532242c0ddaea59aae3823000af9
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-07-07 23:23:37 -0700

    added card component file and imported to app.js

--- donfernando-client/src/App.js
+++ donfernando-client/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import SignUpForm from "./components/SignupForm.js";
 import Header from "./components/Header.js";
+import MembershipDetailCard from "./components/MembershipDetailCard.js";
 
 function App() {
   return (
     <div>
       <Header />
+      <MembershipDetailCard />
       <SignUpForm />{" "}
     </div>
   );
