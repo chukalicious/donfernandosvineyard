15c80237c99ba467457533c37534676a85534f70
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-07-09 20:51:08 -0700

    completed form fields and submit button

--- donfernando-client/src/components/SignupForm.js
+++ donfernando-client/src/components/SignupForm.js
@@ -1,13 +1,26 @@
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 const SignUpForm = () => {
   const provisionaryStyle = {
     marginTop: "1rem",
   };
+
+  const buttonStyles = {
+    width: "60%",
+    margin: "3rem auto",
+    padding: ".5rem",
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submit");
+  };
+
   return (
     <Box
-      component="form"
       sx={{
         paddingTop: "1rem",
         display: "flex",
@@ -16,40 +29,76 @@ const SignUpForm = () => {
         maxWidth: "45%",
       }}
     >
-      <TextField
-        style={provisionaryStyle}
-        required
-        id="firstName"
-        name="firstName"
-        label="First name"
-      />{" "}
-      <TextField
-        style={provisionaryStyle}
-        required
-        id="lastName"
-        name="lastName"
-        label="Last name"
-      />{" "}
-      <TextField
-        style={provisionaryStyle}
-        id="phoneNumber"
-        name="phoneNumber"
-        label="Phone Number"
-      />{" "}
-      <TextField
-        style={provisionaryStyle}
-        required
-        id="emailAddress"
-        name="emailAddress"
-        label="Email Address"
-      />{" "}
-      <TextField
-        required
-        style={provisionaryStyle}
-        id="homeAddress"
-        name="homeAddress"
-        label="Home Address"
-      />{" "}
+      <Typography>Ready to join our club and enjoy more wine?! </Typography>
+      <Typography>
+        Fill in how much wine you want and how to get it to you below!{" "}
+      </Typography>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          style={provisionaryStyle}
+          required
+          id="firstName"
+          name="firstName"
+          label="First name"
+        />{" "}
+        <TextField
+          style={provisionaryStyle}
+          required
+          id="lastName"
+          name="lastName"
+          label="Last name"
+        />{" "}
+        <TextField
+          style={provisionaryStyle}
+          required
+          id="emailAddress"
+          name="emailAddress"
+          label="Email Address"
+        />{" "}
+        <TextField
+          required
+          style={provisionaryStyle}
+          id="homeAddressLine1"
+          name="homeAddressLine1"
+          label="Home Address Line 1"
+        />{" "}
+        <TextField
+          style={provisionaryStyle}
+          id="homeAddressLine2"
+          name="homeAddressLine"
+          label="Home Address Line 2"
+        />{" "}
+        <TextField
+          required
+          style={provisionaryStyle}
+          id="city"
+          name="city"
+          label="City"
+        />{" "}
+        <TextField
+          required
+          style={provisionaryStyle}
+          id="state"
+          name="state"
+          label="State"
+        />{" "}
+        <TextField
+          required
+          style={provisionaryStyle}
+          id="zipCode"
+          name="zipCode"
+          label="Zip Code"
+        />{" "}
+        <TextField
+          style={provisionaryStyle}
+          id="phoneNumber"
+          name="phoneNumber"
+          label="Phone Number"
+        />{" "}
+        <Button type="submit" style={buttonStyles} variant="contained">
+          Submit
+        </Button>
+      </form>
     </Box>
   );
 };
