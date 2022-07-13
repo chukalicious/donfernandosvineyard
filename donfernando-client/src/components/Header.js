77b2517ac291949cb67b31d1cc653d1c25457681
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-07-13 00:00:47 -0700

    Replaced the menu Icon in Header.js with a NavMenu component, removed the login button and the text in the heading.

--- donfernando-client/src/components/Header.js
+++ donfernando-client/src/components/Header.js
@@ -1,30 +1,19 @@
-import * as React from "react";
+import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
+import NavMenu from "./NavMenu";
 
 const Header = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            Dos Fernandos Vineyard
           </Typography>
-          <Button color="inherit">Login</Button>
+          <NavMenu />
         </Toolbar>
       </AppBar>
     </Box>
