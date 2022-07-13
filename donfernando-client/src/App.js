ae48c63139db61abbaafdf29e85419a0fe78aa0f
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-07-13 16:56:14 -0700

    Created routes to components home, sing up form, membership details, about us and awards in App.js. Also created Home component that houses all mentioned components except of course, Home.

--- donfernando-client/src/App.js
+++ donfernando-client/src/App.js
@@ -1,19 +1,32 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header.js";
+import Home from "./components/Home";
 import MembershipDetailCards from "./components/MembershipDetailCards.js";
 import SignUpForm from "./components/SignupForm.js";
 import About from "./components/About";
 import Awards from "./components/Awards";
 import Footer from "./components/Footer";
+import { Fragment } from "react";
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
-      <MembershipDetailCards />
-      <SignUpForm /> <About /> <Awards />
+      <Routes>
+        <Fragment>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<SignUpForm />} />
+          <Route
+            path="/membershipDetails"
+            element={<MembershipDetailCards />}
+          />
+          <Route path="/about" element={<About />} />
+          <Route path="/awards" element={<Awards />} />
+        </Fragment>
+      </Routes>
       <Footer />
-    </div>
+    </>
   );
 }
 
---
+++ donfernando-client/src/components/Home.js
@@ -0,0 +1,14 @@
+import MembershipDetailCards from "./MembershipDetailCards";
+import SignUpForm from "./SignupForm";
+import About from "./About";
+import Awards from "./Awards";
+
+const Home = () => {
+  return (
+    <>
+      <MembershipDetailCards /> <SignUpForm /> <About /> <Awards />
+    </>
+  );
+};
+
+export default Home;
